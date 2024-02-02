8f4f2746e170eece529cede0a8b2959ce151a685
Ali Raza Butt <buttbro578@gmail.com>
2024-02-02 10:39:57 +0500

    complete

---
+++ Backend/Config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    database: "e-commerace",
+    username: "postgres",
+    password: "96869686",
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+  },
+  jwtSecret: "aaaa",
+};
